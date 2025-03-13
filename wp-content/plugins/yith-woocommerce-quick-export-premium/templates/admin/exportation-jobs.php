41a8dc68de88adc4cf45c2ffa4a4160b544e2efd
Tecnologia-apadrina <tecnologia@apadrinaunolivo.org>
2025-03-13 15:10:41 +0100

    local-botones

--- wp-content/plugins/yith-woocommerce-quick-export-premium/templates/admin/exportation-jobs.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET["create-job"])) :
-	include( YITH_YWQE_TEMPLATES_DIR . 'admin/create-job.php' );
-else:
-	include( YITH_YWQE_TEMPLATES_DIR . 'admin/jobs-table.php' );
-endif;
-?>
